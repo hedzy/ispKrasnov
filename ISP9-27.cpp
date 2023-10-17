9b0f32fc951c8928ad20753dc7edc38cc820969d
hedzy <hedzy@yandex.ru>
2023-10-17 16:41:54 +0300

    commit1

---
+++ ISP9-27.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    const int h = 24;
+    std::cout << "Введите количество дней";
+    int d;
+    std::cin >> d;
+    std::cout << (d*h) + " часов в " + d;
+}
